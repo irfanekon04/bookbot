56b6560966ef204328a6423f6dc68132c8b43212
irfanekon <irfanekon2004@gmail.com>
2026-09-22 00:47:39 +0600

    complete

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from stats import wordCount, getCharCount
+import sys
+from stats import wordCount, getCharCount, charsDictToSortedList
 def get_book_text(filepath:str) -> str:
     with open(filepath) as f:
         file_contents:str = f.read()
         return file_contents
 
 
+def printReport(path: str,wordCount: int, sortedCharList: list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {wordCount} total words")
+
+    print("--------- Character Count -------")
+    for char,count in sortedCharList:
+        if not char.isalpha():
+            continue
+        print(f"{char}: {count}" )
+    print("============= END ===============")
+
 def main()-> None:
-    file_contents :str = get_book_text("books/frankenstein.txt")
+    if (len(sys.argv)<2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    bookPath = sys.argv[1]
+    file_contents :str = get_book_text(bookPath)
     wordsCount = wordCount(file_contents)
-    print(f"Found {wordsCount} total words")
-    print(getCharCount(file_contents.lower()))
+    sortedList = charsDictToSortedList(getCharCount(file_contents.lower()))
+    printReport(bookPath,wordsCount,sortedList)
 
 main()
--- stats.py
+++ stats.py
@@ -2,6 +2,19 @@ def wordCount(text: str) -> int:
     return len(text.split())
 
 
+def sort_on(words : tuple[str,int]):
+    return words[1]
+
+
+def charsDictToSortedList(characterCount: dict[str,int]):
+    charCount = []
+    for keys in characterCount:
+        count = characterCount[keys]
+        charCount.append((keys,count))
+    sortedCharCount = sorted(charCount,reverse=True,key=sort_on)
+    return sortedCharCount
+
+
 def getCharCount(text: str) -> dict[str,int]:
     characterCount: dict[str,int] = {}
     for c in text:
